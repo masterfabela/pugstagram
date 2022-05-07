10651f7b72316d0d67888a33ef99a97609b26088
Jose Ramon Vieites Romay <jose.vieites@altia.es>
2022-05-07 21:24:43 +0200

    Styles applied

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -3,3 +3,12 @@
     <div class="Footer-copy">2020 PUGSTAGRAM FROM PLATZI</div>
   </div>
 </div>
+
+<style>
+  .Footer {
+    font-size: 11px;
+    letter-spacing: 1px;
+    font-weight: normal;
+    color: #c7c7c7;
+  }
+</style>
